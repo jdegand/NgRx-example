124ed9a3637e21ccd6c795da103374957d524f1c
J. Degand <70610011+jdegand@users.noreply.github.com>
2025-09-21 22:14:59 +0000

    fix: remove commented out code and add readonly to book effects

--- src/app/state/books.effects.ts
+++ src/app/state/books.effects.ts
@@ -19,36 +19,7 @@ export class BooksEffects {
   );
  
   constructor(
-    private actions$: Actions,
-    private GoogleBooksService: GoogleBooksService
+    private readonly actions$: Actions,
+    private readonly GoogleBooksService: GoogleBooksService
   ) {}
 }
-
-// this.store.dispatch(BooksApiActions.retrievedBookList({ books })
-
-/*
-import { Injectable } from '@angular/core';
-import { Actions, createEffect, ofType } from '@ngrx/effects';
-import { EMPTY } from 'rxjs';
-import { map, exhaustMap, catchError } from 'rxjs/operators';
-import { GoogleBooksService } from '../book-list/books.service';
-
-@Injectable()
-export class BooksEffects {
- 
-  loadBooks$ = createEffect(() => this.actions$.pipe(
-    ofType('[Books Page] Load Books'),
-    exhaustMap(() => this.GoogleBooksService.getBooks()
-      .pipe(
-        map(books => ({ type: '[Books API] Books Loaded Success', payload: books })),
-        catchError(() => EMPTY)
-      ))
-    )
-  );
- 
-  constructor(
-    private actions$: Actions,
-    private GoogleBooksService: GoogleBooksService
-  ) {}
-}
-*/
